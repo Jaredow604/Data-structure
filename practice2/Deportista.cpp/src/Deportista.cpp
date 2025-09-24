ac8fecd647e62e0e7d7a127de28e1061d62efcf1
Jaredow604 <143060313+Jaredow604@users.noreply.github.com>
2025-09-24 17:30:34 -0600

    Update Deportista.cpp
    update the Deportista class

--- practice2/Deportista.cpp/src/Deportista.cpp
+++ practice2/Deportista.cpp/src/Deportista.cpp
@@ -1,8 +1,7 @@
 #include "Deportista.h"
 #include <string>
-#include <cstring> // Para strncpy
+#include <cstring> 
 
-// Constructor de la clase Deportista
 Deportista::Deportista(int numeroSocio, const std::string& nombreDeportista, int edadDeportista, const std::string& disciplinaDeportista)
 {
     establecerNoSocio(numeroSocio);
@@ -10,59 +9,49 @@ Deportista::Deportista(int numeroSocio, const std::string& nombreDeportista, int
     establecerEdad(edadDeportista);
     establecerDisciplina(disciplinaDeportista);
 }
-
-// Obtiene el número de socio
 int Deportista::obtenerNoSocio() const
 {
     return noSocio;
 }
-
-// Establece el número de socio
 void Deportista::establecerNoSocio(int numeroSocio)
 {
     noSocio = numeroSocio;
 }
 
-// Obtiene el nombre del deportista
 std::string Deportista::obtenerNombre() const
 {
     return nombre;
 }
 
-// Establece el nombre del deportista
 void Deportista::establecerNombre(const std::string& cadenaNombre)
 {
     const char *valorNombre = cadenaNombre.data();
     int longitud = cadenaNombre.size();
     longitud = (longitud < 30 ? longitud : 29);
     strncpy(nombre, valorNombre, longitud);
-    nombre[longitud] = '\0'; // Agrega el carácter nulo de terminación
+    nombre[longitud] = '\0'; 
 }
 
-// Obtiene la edad del deportista
 int Deportista::obtenerEdad() const
 {
     return edad;
 }
 
-// Establece la edad del deportista
 void Deportista::establecerEdad(int edadDeportista)
 {
     edad = edadDeportista;
 }
 
-// Obtiene la disciplina del deportista
 std::string Deportista::obtenerDisciplina() const
 {
     return disciplina;
 }
 
-// Establece la disciplina del deportista
 void Deportista::establecerDisciplina(const std::string& cadenaDisciplina)
 {
     const char *valorDisciplina = cadenaDisciplina.data();
     int longitud = cadenaDisciplina.size();
     longitud = (longitud < 20 ? longitud : 19);
     strncpy(disciplina, valorDisciplina, longitud);
-    disciplina[longitud] = '\0'; // Agrega el carácter nulo de terminación
+    disciplina[longitud] = '\0'; 
 }
