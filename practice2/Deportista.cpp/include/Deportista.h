9d0cb19f7ba3db95765b3b199757896178c1a516
Jaredow604 <143060313+Jaredow604@users.noreply.github.com>
2025-09-24 17:33:38 -0600

    Update Deportista.h
    update "Deportista" class

--- practice2/Deportista.cpp/include/Deportista.h
+++ practice2/Deportista.cpp/include/Deportista.h
@@ -6,22 +6,13 @@
 class Deportista
 {
 public:
-    // Constructor predeterminado
     Deportista(int = 0, const std::string& = "", int = 0, const std::string& = "");
-
-    // Funciones de acceso para el número de socio
     void establecerNoSocio(int);
     int obtenerNoSocio() const;
-
-    // Funciones de acceso para el nombre
     void establecerNombre(const std::string&);
     std::string obtenerNombre() const;
-
-    // Funciones de acceso para la edad
     void establecerEdad(int);
     int obtenerEdad() const;
-
-    // Funciones de acceso para la disciplina
     void establecerDisciplina(const std::string&);
     std::string obtenerDisciplina() const;
 
